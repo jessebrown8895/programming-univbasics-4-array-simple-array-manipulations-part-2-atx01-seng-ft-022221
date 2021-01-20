85832ec9b0d42cecd22904f7154f888ef833e46d
Jesse Brown <brownjesse8895@yahoo.com>
2021-01-20 21:10:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,7 +6,9 @@ def using_concat (my_favorite_things, more_favs)
    new_array = list_of_programming_languages.insert(4, another_language)
  end
  
- def using_uniq()
+ def using_uniq(haircuts)
+   haircuts.uniq 
+ end
   
   
 
