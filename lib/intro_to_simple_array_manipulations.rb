5696500b4a59576d9745bccaf41abf9e75cc8f40
Jesse Brown <brownjesse8895@yahoo.com>
2021-01-20 21:04:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,9 @@ def using_concat (my_favorite_things, more_favs)
  my_favorite_things.concat(more_favs)
   end 
   
- def using_insert
-   
+ def using_insert(list_of_programming_languages, another_language)
+    @new_array = using_insert(@list_of_programming_languages, @another_language)
+    new_array = 
  end
   
   
